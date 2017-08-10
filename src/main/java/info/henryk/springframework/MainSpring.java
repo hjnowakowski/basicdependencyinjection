2fdd89f885140e2c5ce2bf3829cfb9f165a5618c
henryknowakowski <DZ5335516GWhj+n7991>
2017-08-10 08:07:48 +0200

    Tworzenie metod fabrykujacych

--- src/main/java/info/henryk/springframework/MainSpring.java
+++ src/main/java/info/henryk/springframework/MainSpring.java
@@ -1,15 +1,16 @@
 package info.henryk.springframework;
 
-import info.henryk.springframework.api.UserRepository;
-import info.henryk.springframework.implementations.SomeBean;
-import org.springframework.context.ApplicationContext;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainSpring {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        SomeBean bean = context.getBean("someBean", SomeBean.class);
+        User user = context.getBean("someUser", User.class);
+        System.out.println(user);
+
 
     }
+
 }
