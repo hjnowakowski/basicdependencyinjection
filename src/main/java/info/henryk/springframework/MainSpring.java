8db5f55cd9c65c45f825fc9c7c6f6d45ecb7f9f5
henryknowakowski <DZ5335516GWhj+n7991>
2017-08-16 16:11:57 +0200

    wstrzykiwanie list, tablic i zbirów

--- src/main/java/info/henryk/springframework/MainSpring.java
+++ src/main/java/info/henryk/springframework/MainSpring.java
@@ -4,12 +4,19 @@ package info.henryk.springframework;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+
 public class MainSpring {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        User user = context.getBean("user2", User.class);
+        //User user = context.getBean("user2", User.class);
 
-        System.out.println(user);
+        //System.out.println(user);
+
+        SomeBean bean = context.getBean("someBean", SomeBean.class);
+        System.out.println(bean.getIntegersList());
+        System.out.println(bean.getStringList());
+        System.out.println(Arrays.toString(bean.getLongArray()));
 
     }
 }
