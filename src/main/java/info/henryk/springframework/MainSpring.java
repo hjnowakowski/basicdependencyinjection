3e529e2e8f017f21176964de322ca9d2e2c4e0a7
henryknowakowski <DZ5335516GWhj+n7991>
2017-08-17 21:33:55 +0200

    moment inicjalizacji beana i odraczanie inicjalizacji

--- src/main/java/info/henryk/springframework/MainSpring.java
+++ src/main/java/info/henryk/springframework/MainSpring.java
@@ -9,6 +9,7 @@ public class MainSpring {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
+        context.getBean("someBean", SomeBean.class);
 
 
 
--- src/main/java/info/henryk/springframework/SomeBean.java
+++ src/main/java/info/henryk/springframework/SomeBean.java
@@ -1,26 +1,10 @@
 package info.henryk.springframework;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.InitializingBean;
 
-import java.util.Map;
+public class SomeBean implements InitializingBean {
 
-public class SomeBean implements BeanNameAware, ApplicationContextAware {
-    private String name;
-    private ApplicationContext applicationContext;
-
-    public void setBeanName(String name) {
-        this.name = name;
-    }
-
-    private void init(){
-        Map<String, SomeBean> beansOfType = applicationContext.getBeansOfType(SomeBean.class);
-        System.out.println("Na zywam się " + name + "Jest nas już: " + beansOfType.size());
-    }
-
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Inicjalizacja");
     }
 }
